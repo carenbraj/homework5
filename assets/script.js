76a79f4ad040266912163dbe399b2f69dbc4c251
Caren Braj <carenbraj@gmail.com>
2020-08-04 15:04:37 -0700

    final

--- assets/script.js
+++ assets/script.js
@@ -1,76 +1,74 @@
 // Build HTML - bootstrap
 
-// Grab current date for the top of the scheduler - moment JS
-function getLocalStorage() {
-    var value = localStorage.getItem();
-
-    }
 // Grab current hours for scheduler - moment.hours
 $(document).ready(function() {
     $("#currentDay").text(moment().format("MMMM Do YYYY, h:mm:ss a")) 
 
-var hours = ["8am", "9am", "10am", "11am", "12pm", "1pm", "2pm", "3pm", "4pm", "5pm"]
-var colorChanges = ["8am", "9am", "10am", "11am", "12pm", "1pm", "2pm", "3pm", "4pm", "5pm"]
-var i = 0
+var showHours = ["8am", "9am", "10am", "11am", "12pm", "1pm", "2pm", "3pm", "4pm", "5pm"]
+var hours = ["8", "9", "10", "11", "12", "13", "14", "15", "16", "17"]
+
 
 
 // Add hours to .container
 for (var i = 0; i < hours.length; i++) {
-   var newRow = $("<div class='row'></div>")
+   var newRow = $(`<div class='row time-blocks' id=${hours[i]}></div>`)
    $(".container").append(newRow)
-   var hourCol = $(`<div class='col-1 time-block'>${hours[i]}</div>`)
-   var text = $(`<textarea class="col-10" data-time=${colorChanges[i]}>Enter your notes here...</textarea>`)
+   var hourCol = $(`<div class='col-1 time-block'>${showHours[i]}</div>`)
+   var text = $(`<textarea class="col-10 description" data-time=${hours[i]}>Enter your notes here...</textarea>`)
    var buttonCol = $('<button class="col-1 saveBtn">Save</button>')
    $(newRow).append(hourCol, text, buttonCol);
 }
 
 // One click event for save button and put event on that class
 $(".saveBtn").on("click", function () {
-    console.log(this)
-    var eventText = $(this).attr("data-value")
-    var eventId = $(click).val()
-    localStorage.setItem(eventText, eventId)
+    var value = $(this).siblings(".description").val()
+    var time = $(this).parent().attr("id")
+    localStorage.setItem(time, value)
 })
+
+// var obj = {name: Caren, age: 28}
     
 
 // Color coded hours - store numeric value in localStorage
 function colorChange() {
-    $("textarea").each(function(){
-        var currentHour = parseInt(moment().hours())
-        var textArea = $('textarea').data('time')
-        if (textArea , currentHour) {
-            $('textarea').removeClass("present")
-            $('textarea').removeClass("future")
-            $('textarea').addClass("past")
+    $(".description").each(function(){
+        var currentHour = moment().hours()
+        var textArea = parseInt($(this).data("time"))
+        console.log(currentHour);
+        if (textArea < currentHour) {
+            $(this).addClass("past")
         }
         else if (textArea === currentHour) {
-            $('textarea').removeClass("past")
-            $('textarea').removeClass("future")
-            $('textarea').addClass("present")
+            $(this).removeClass("past")
+            $(this).addClass("present")
         }
         else {
-            $('textarea').removeClass("past")
-            $('textarea').removeClass("present")
-            $('textarea').addClass("future")
+            $(this).removeClass("past")
+            $(this).removeClass("present")
+            $(this).addClass("future")
         }
-
+    
     });
 };
 colorChange();
+
+$("#8 .description").val(localStorage.getItem("8"));
+$("#9 .description").val(localStorage.getItem("9"));
+$("#10 .description").val(localStorage.getItem("10"));
+$("#11 .description").val(localStorage.getItem("11"));
+$("#12 .description").val(localStorage.getItem("12"));
+$("#13 .description").val(localStorage.getItem("13"));
+$("#14 .description").val(localStorage.getItem("14"));
+$("#15 .description").val(localStorage.getItem("15"));
+$("#16 .description").val(localStorage.getItem("16"));
+$("#17 .description").val(localStorage.getItem("17"));
+
+
 })
 
 
 
 
-// Set interval timer that updates page every hour
 
-// JS- capture all scheduleInput boxes $() input - gives array of all scheduleInput boxes and then you can do .each 
 
-// Loop through scheduleInput box - look at numeric value - If Statement if current time is > row time = add class past  else = present
 
-// One click event for save button and put event on that class
-
-// .THIS will give you the element you clicked and the DOM triversal for which scheduleInput matches
-
-// Save in local storage
-// When page loads stored items in local storage are grabed and displayed on page.
